ff0cf354b218af882be9ee5bb680cad8a3290359
sachin0410 <guptasachinofficial@gmail.com>
2016-08-22 22:22:02 +0530

    Add existing file

--- app/src/main/java/com/example/sachingupta/test3/OnSelectDuScreen.java
+++ app/src/main/java/com/example/sachingupta/test3/OnSelectDuScreen.java
@@ -35,117 +35,18 @@ public class OnSelectDuScreen extends Activity implements
     public void onItemSelected(AdapterView<?> arg0, View view, int arg2,
                                long arg3) {
         // TODO Auto-generated method stub
-
-        String sp1 = String.valueOf(arg0.getSelectedItem());
-        //Toast.makeText(this, sp1, Toast.LENGTH_SHORT).show();
-        if (sp1.contentEquals("I")) {
-            List<String> list = new ArrayList<String>();
-            list.add("-----");
-            List<String> list1 = new ArrayList<String>();
-            list1.add("Applied Mathematics-I ");
-            list1.add("Applied Physics-I");
-            list1.add("Manufacturing Processes");
-            list1.add("Electrical Technology");
-            list1.add("Human Values and Professional Ethics-I# ");
-            list1.add("Fundamentals of Computing");
-            list1.add("Applied Chemistry ");
-
+        s1.getSelectedItem().toString();
+        if  (arg0 == s1)
+        {
+            DataBaseHelper db = new DataBaseHelper(getApplicationContext());
+            String[] branches = db.getBranchesName(s1.getSelectedItem().toString());
             ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, list);
+                    android.R.layout.simple_spinner_item, branches);
             dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             dataAdapter.notifyDataSetChanged();
             s2.setAdapter(dataAdapter);
-            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, list1);
-            dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            dataAdapter2.notifyDataSetChanged();
-            s3.setAdapter(dataAdapter2);
-        } else if (sp1.contentEquals("II")) {
-            List<String> list = new ArrayList<String>();
-            list.add("-----");
-            List<String> list1 = new ArrayList<String>();
-            list1.add("Applied Mathematics-II ");
-            list1.add("Applied Physics-II");
-            list1.add("Electronic Devices");
-            list1.add("Introduction to Programming ");
-            list1.add("Environmental Studies");
-            list1.add("Communication Skills ");
-            list1.add("Engineering Mechanics ");
-
-            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, list);
-            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            dataAdapter.notifyDataSetChanged();
-            s2.setAdapter(dataAdapter);
-            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, list1);
-            dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            dataAdapter2.notifyDataSetChanged();
-            s3.setAdapter(dataAdapter2);
         }
-        else if (sp1.contentEquals("III")) {
-            List<String> list = new ArrayList<String>();
-            list.add("Computer Science");
-            list.add("Information Technology");
-            list.add("Mechanical");
-            list.add("Electrical and Electronics");
-            list.add("Electronics Communication");
-            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, list);
-            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            dataAdapter.notifyDataSetChanged();
-            s2.setAdapter(dataAdapter);
 
-            if (sp1.contentEquals("Computer Science")) {
-                List<String> list1 = new ArrayList<String>();
-                list1.add("Applied Mathematics-III ");
-                list1.add("Foundation of Computer Science ");
-                list1.add("Switching Theory and Logic Design ");
-                list1.add("Circuits and Systems ");
-                list1.add("Data Structure ");
-                list1.add("Computer Graphics and Multimedia ");
-                ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
-                        android.R.layout.simple_spinner_item, list1);
-                dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                dataAdapter2.notifyDataSetChanged();
-                s3.setAdapter(dataAdapter2);
-
-
-            } else if (sp1.contentEquals("Mechanical")) {
-                List<String> list1 = new ArrayList<String>();
-                list1.add("Numerical Analysis and Statistical Techniques");
-                list1.add("Electrical Machines ");
-                list1.add("Thermal Science ");
-                list1.add("Production Technology ");
-                list1.add("Material Science and Metallurgy");
-                list1.add("Strength of Materials-I ");
-                ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
-                        android.R.layout.simple_spinner_item, list1);
-                dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                dataAdapter2.notifyDataSetChanged();
-                s3.setAdapter(dataAdapter2);
-
-
-            }
-
-        }
-        else if (sp1.contentEquals("Computer Science")) {
-            if (s1.getSelectedItem().toString().contentEquals("III") )
-            {
-                List<String> list1 = new ArrayList<String>();
-                list1.add("Applied Mathematics-III ");
-                list1.add("Foundation of Computer Science ");
-                list1.add("Switching Theory and Logic Design ");
-                list1.add("Circuits and Systems ");
-                list1.add("Data Structure ");
-                list1.add("Computer Graphics and Multimedia ");
-                ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
-                        android.R.layout.simple_spinner_item, list1);
-                dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                dataAdapter2.notifyDataSetChanged();
-                s3.setAdapter(dataAdapter2);
-            }
-        }
     }
 
     @Override
